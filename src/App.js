7572ea94bc8175c34960caee047444c9eca2a720
Morozov Ivan <morozoff.one@yandex.ru>
2024-09-22 23:28:50 +0300

    created counter of years on react with old method with Class and Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  class App extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        years: 27,
+      }
+    }
+
+    nextYear = () => {
+      this.setState(state => ({
+        years: state.years + 1
+      }))
+    }
+
+    prevYear = () => {
+      this.setState(state => ({
+        years: state.years - 1,
+      }))
+    }
+  render() {
+    const {name, surname} = this.props;
+    return (
+      <div className='App'>
+          <h1>{name} {surname}</h1>
+          <div>{this.state.years}</div>
+          <button onClick={this.nextYear}>+</button>
+          <button onClick={this.prevYear}>-</button>
+      </div>
+      
+    )
+  }
+
+  }
+
 
 export default App;
